0dc804ae89a1e587ea03a0350b095f2456980641
SSmln <lsm7630@gmail.com>
2024-03-13 19:38:30 +0900

    asdasd123

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
+// /** @type {import('next').NextConfig} */
+// const debug = process.env.NODE_ENV !== "production";
+// const repository = "CEMI_next";
+
+// const nextConfig = {
+//   assetPrefix:
+//     process.env.NODE_ENV === "production"
+//       ? "https://ssmln.github.io/cemi-next"
+//       : "",
+//   output: "export",
+
+// };
+
+// export default nextConfig;
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+    trailingSlash: true,
+    images: {
+        unoptimized: true
+    }
+}
+
+    ;
 
 export default nextConfig;
